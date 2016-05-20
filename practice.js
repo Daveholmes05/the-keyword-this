f5cc05c8ae65669c1eb9d7b49bc5a54b9388da6a
Dave Holmes <daveholmes05@gmail.com>
2016-05-19 20:40:40 -0600

    finished

--- practice.js
+++ practice.js
@@ -1,19 +1,23 @@
 //We're in a job interview. Answer the following questions (try to not look at your notes unless you have to).
   // 1) What is the purpose of the 'this keyword'?
 
-      //Answer
+      //It's a referant. It allows you to point to an object and use values from that object.
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
-      //Answer
+      //default - window object. If it doesn't find the object it will keep going until it hits the window object.
+      //call - calls the object. You have to pass the parameters individually
+      //apply - adds to the object? You can pass the array variable and it will list all the values
+      //bind - can't remember
 
   // 3) What is the difference between call and apply?
 
-      //Answer
+      //With call you have to pass the arguments as parameters individually. With apply you can pass the variable array
+      //as the parameter.
 
   // 4) What does .bind do?
 
-      //Answer
+      //Not sure.
 
 
 //Next Problem
@@ -24,24 +28,40 @@
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
     //Code Here
-
+var user = {
+  username: 'UserNameMan',
+  email: 'test123@test.com',
+  getUsername: function() {
+    return this.username;
+  }
+};
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
-
+user.getUsername();
 
 //Next Problem
 
 
 // Write the function definitions which will make the following function invocations function properly.
-
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.move = 0;
+  this.moveCar = function() {
+    this.move += 10;
+    return this.move;
+  };
+}
   //Function Invocations Here
-
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
 
 prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
 mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
 
-//Hint, you'll need to write a moveCar function which is added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
+//Hint, you'll need to write a moveCar function which is added to every object that is being returned from the Car 
+//function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right
+// object (prius vs mustang).
 
 
 
